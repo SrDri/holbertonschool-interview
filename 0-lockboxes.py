f1afb1ba0050fb5742395f0e2e82b010dddf1c33
SrDri <juanjcarabali@gmail.com>
2021-12-09 12:43:55 +0700

    Update project

---
+++ 0-lockboxes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Lockboxes """
+
+def canUnlockAll(boxes):
+    """ opened or not """
+    keys = [0]
+    for key in keys:
+        for i in boxes[key]:
+            if i not in keys and i < len(boxes):
+                keys.append(i)
+    if len(keys) == len(boxes):
+        return True
+    return False
